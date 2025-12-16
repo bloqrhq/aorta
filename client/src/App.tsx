0f0f82a989f9e4cf2d4f8d74f6853ad90ce8290f
Shankar-CSE <shankar.l5252@gmail.com>
2025-12-16 10:42:00 +0530

    refactor:   remove unused components to merge to main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './dashboard';
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+export default function App() {
+  return (<>
+  <h1 className="underline text-red-500">Welcome to Aorta!</h1>
+  </>);
+}
